f24c17dbbc7a159da361f3f0a0b1e84591a06d3c
Bruce Wayne <91925396+brucelwayne@users.noreply.github.com>
2023-10-12 13:43:48 +0800

    update

---
+++ database/migrations/2023_10_12_000001_create_admins_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    protected $connection = 'mysql';
+
+    public function up(): void
+    {
+        Schema::create('admins', function ($table) {
+            /**
+             * @var Illuminate\Database\Schema\Blueprint|MongoDB\Laravel\Schema\Blueprint $table
+             */
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('admins');
+    }
+};
--- src/AdminServiceProvider.php
+++ src/AdminServiceProvider.php
@@ -2,10 +2,13 @@
 
 namespace Brucelwayne\Admin;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AdminServiceProvider extends ServiceProvider
 {
+
+    protected $module_name = 'admin';
 
     public function register()
     {
@@ -14,5 +17,38 @@ class AdminServiceProvider extends ServiceProvider
 
     public function boot(){
 
+        $this->bootConfigs();
+        $this->bootRoutes();
+        $this->bootMigrations();
+        $this->bootComponentNamespace();
+        $this->loadBladeViews();
+
+    }
+
+    protected function loadBladeViews(): void
+    {
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', $this->module_name);
+    }
+
+    protected function bootComponentNamespace(): void
+    {
+        Blade::componentNamespace('Brucelwayne\\Admin\\View\\Components', $this->module_name);
+    }
+
+    protected function bootConfigs(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/admin.php', $this->module_name
+        );
+    }
+
+    protected function bootRoutes(): void
+    {
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+    }
+
+    protected function bootMigrations(): void
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
     }
 }
