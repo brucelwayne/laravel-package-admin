2697e480c1c4ba46a6c5322435b1017f013167d6
Bruce Wayne <91925396+brucelwayne@users.noreply.github.com>
2023-10-12 13:39:59 +0800

    init

---
+++ src/AdminServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Brucelwayne\Admin;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminServiceProvider extends ServiceProvider
+{
+
+    public function register()
+    {
+
+    }
+
+    public function boot(){
+
+    }
+}
